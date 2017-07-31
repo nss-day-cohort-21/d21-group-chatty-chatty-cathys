b2976e1cabdd4a4ba3906f6aed7ffac61eaec846
Michael Lindstrom <michaellindstr@gmail.com>
2017-07-31 10:30:55 -0500

    created Chatty object with JSON loader function

--- javascripts/loader.js
+++ javascripts/loader.js
@@ -1,1 +1,26 @@
 console.log("loader.js loaded");
+
+{
+  var Chatty = {};
+
+  let loadedJSON = [];
+
+  Chatty.loadJSON = function(callback) {
+    let myR = new XMLHttpRequest();
+    myR.addEventListener("load", function(){
+      loadedJSON = JSON.Parse(this.responseText);
+      callback(loadedJSON)
+    })
+    myR.open("GET", "messages.json")
+    myR.send();
+  }
+}
+
+{
+  Chatty.getMessages = function(messages) {
+    console.log(messages);
+    return messages
+  }
+}
+
+Chatty.loadJSON(Chatty.getMessages);
